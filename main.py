2a6c3f7057bc3f9ee7238edb68bac5056c717dec
Kirill Mashina <you@example.com>
2023-07-19 17:01:37 +0300

    ///

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from aiogram import Bot
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Sonic-Shop")
 
 bot = Bot(token='6256199322:AAFLFp7LVBy7PxPeyN5ylGXonwcZ5p-3HHk')
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
@@ -13,4 +16,4 @@ async def start(request: Request):
     host = str(request.client.host)
     await bot.send_message(587247376, str(request.client.host))
     print(host)
-    return templates.TemplateResponse("main.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
