2782d60c5210ab077e25161cfd24992d5ed611cc
Kirill Mashina <you@example.com>
2023-07-19 16:25:25 +0300

    ///

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from aiogram import Bot
 from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="Sonic-Shop")
 
+bot = Bot(token='6256199322:AAFLFp7LVBy7PxPeyN5ylGXonwcZ5p-3HHk')
+templates = Jinja2Templates(directory="templates")
+
+
 @app.get('/')
-async def add_telegram_user(request: Request):
-    client_host = request.client.host
-    return {"client_host": client_host}
+async def start(request: Request):
+    host = str(request.client.host)
+    await bot.send_message(587247376, str(request.client.host))
+    print(host)
+    return templates.TemplateResponse("main.html", {"request": request})
